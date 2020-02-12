f62717c65dc42ad8eab84a363853634f643b0586
lukas.sin <lukas.sin@kakaocorp.com>
2020-02-12 12:07:13 +0900

    Add router hooks in component handling 1. add hooks in index component 2. add affterEach func

--- vue-cli/src/core/router.js
+++ vue-cli/src/core/router.js
@@ -8,10 +8,19 @@ import HelloWorld from '../components/HelloWorld'
 
 Vue.use(VueRouter)
 
+/**
+ * vue router는
+ * 1. pipe line으로 구성되어 있다 .
+ * 2. 인자 to 어디어디로
+ * 3. 인자 from 어디어디로 부터
+ * 4. 인자 next <-- 요게 navigation update
+ * 5. next 함수의 경우 순환 참가 될 수 있으므로 route 변경 시 순환참조를 막도록 하여야 한다
+ */
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', name: 'home', component: Index },
+    { path: '/', name: 'index', component: Index },
     { path: '/login', name: 'login', component: Login },
     { path: '/home', meta: { requiresAuth: true }, name: 'home', component: Home },
     { path: '/about', meta: { requiresAuth: true }, name: 'about', component: About }, // local navigation guard
@@ -31,6 +40,17 @@ const _checkRequiresAuth = (isAuth, to, next, hndr) =>
 router.beforeEach((to, from, next) => {
   let isAuth = false // vuex data bindings
   _checkRequiresAuth(isAuth, to, next, _checkAuth)
+})
+
+// afterEach
+/**
+ * 1. 라우터에서 제공하는 hooks 들을 promise 실행 후 실행 된다.
+ * 2. 라우트 이동 완료 후 실행
+ * 3. next 가 제공 되지 않아서 navigation update를 실행 하지 않는다.
+ * 4. 라우터 객체의 변화가 있어야만 실행 됨.
+ */
+router.afterEach((to, from) => {
+  console.log('afterEach \n', 'to', to, '\n', 'from', from)
 })
 
 // navigation guard others...
--- vue-cli/src/pages/Index.vue
+++ vue-cli/src/pages/Index.vue
@@ -13,6 +13,22 @@ export default {
   name: 'Index',
   components: {
     TodoComponent: TodoComponent
+  },
+  /**
+   * 컴포넌트에서의 navigation guard hooks
+   */
+  beforeRouteEnter(to, from,next) {
+    // 이 컴포넌트를 렌더링하는 라우트 앞에 호출됩니다.
+    // 이 가드가 호출 될 때 아직 생성되지 않았기 때문에
+    // `this` 컴포넌트 인스턴스에 접근 할 수 없습니다!
+    console.log('beforeRouteEnter',to,from)
+    next()
+  },
+  beforeRouteLeave(to, from,next) {
+    // 이 컴포넌트를 렌더링하는 라우트가 이전으로 네비게이션 될 때 호출됩니다.
+    // `this` 컴포넌트 인스턴스에 접근 할 수 있습니다.
+    console.log('beforeRouteLeave',to,from)
+    next()
   }
 }
 </script>
